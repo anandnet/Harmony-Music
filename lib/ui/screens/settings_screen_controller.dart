2caa965cc915fe3124882dac56c8bae783126722
Anand kumar <anandnet628@gmail.com>
2023-08-12 13:21:07 +0530

    New version 1.3.2, Fixed 403 error by upgrading package

--- lib/ui/screens/settings_screen_controller.dart
+++ lib/ui/screens/settings_screen_controller.dart
@@ -17,7 +17,7 @@ class SettingsScreenController extends GetxController {
   final isIgnoringBatteryOptimizations = false.obs;
   final discoverContentType = "QP".obs;
   final isNewVersionAvailable = false.obs;
-  final currentVersion = "V1.3.1";
+  final currentVersion = "V1.3.2";
   @override
   void onInit() {
     _setInitValue();

9eb9664f72e8c35283973f0d48888953accdc634
Anand kumar <anandnet628@gmail.com>
2023-12-29 00:25:08 +0530

    Search screen modified and added query history fearure

--- lib/ui/screens/Search/search_screen.dart
+++ lib/ui/screens/Search/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:harmonymusic/ui/screens/Settings/settings_screen_controller.dart';
 
 import '/ui/navigator.dart';
 import 'search_screen_controller.dart';
@@ -10,104 +11,152 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final searchScreenController = Get.put(SearchScreenController());
+    final settingsScreenController = Get.find<SettingsScreenController>();
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Row(
-        children: [
-          Container(
-            width: 60,
-            color: Theme.of(context).navigationRailTheme.backgroundColor,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.arrow_back_ios_new_rounded,
-                      color: Theme.of(context).textTheme.titleMedium!.color,
+      body: Obx(
+        () => Row(
+          children: [
+            settingsScreenController.isBottomNavBarEnabled.isFalse
+                ? Container(
+                    width: 60,
+                    color:
+                        Theme.of(context).navigationRailTheme.backgroundColor,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 80),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_back_ios_new_rounded,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .color,
+                            ),
+                            onPressed: () {
+                              Get.nestedKey(ScreenNavigationSetup.id)!
+                                  .currentState!
+                                  .pop();
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                    onPressed: () {
-                      Get.nestedKey(ScreenNavigationSetup.id)!
-                          .currentState!
-                          .pop();
-                    },
+                  )
+                : const SizedBox(
+                    width: 15,
                   ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 90, left: 5),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "search".tr,
-                      style: Theme.of(context).textTheme.titleLarge,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 90, left: 5),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "search".tr,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    textCapitalization: TextCapitalization.sentences,
-                    controller: searchScreenController.textInputController,
-                    textInputAction: TextInputAction.search,
-                    onChanged: searchScreenController.onChanged,
-                    onSubmitted: (val) {
-                      Get.toNamed(ScreenNavigationSetup.searchResultScreen,
-                          id: ScreenNavigationSetup.id, arguments: val);
-                    },
-                    autofocus: true,
-                    cursorColor: Theme.of(context).textTheme.bodySmall!.color,
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.only(left: 5),
-                      focusColor: Colors.white,
-                      hintText: "searchDes".tr,
+                    const SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  Expanded(
-                      child: Obx(() => ListView.builder(
-                            padding: const EdgeInsets.only(top: 5),
-                            physics: const BouncingScrollPhysics(
-                                parent: AlwaysScrollableScrollPhysics()),
-                            itemCount:
-                                searchScreenController.suggestionList.length,
-                            itemBuilder: (context, index) => ListTile(
-                              contentPadding:
-                                  const EdgeInsets.only(left: 10, right: 20),
-                              onTap: () {
-                                Get.toNamed(
-                                    ScreenNavigationSetup.searchResultScreen,
-                                    id: ScreenNavigationSetup.id,
-                                    arguments: searchScreenController
-                                        .suggestionList[index]);
-                              },
-                              title: Text(
-                                  searchScreenController.suggestionList[index]),
-                              trailing: InkWell(
-                                onTap: () {
-                                  searchScreenController.suggestionInput(
+                    TextField(
+                      textCapitalization: TextCapitalization.sentences,
+                      controller: searchScreenController.textInputController,
+                      textInputAction: TextInputAction.search,
+                      onChanged: searchScreenController.onChanged,
+                      onSubmitted: (val) {
+                        Get.toNamed(ScreenNavigationSetup.searchResultScreen,
+                            id: ScreenNavigationSetup.id, arguments: val);
+                        searchScreenController.addToHistryQueryList(val);
+                      },
+                      autofocus: true,
+                      cursorColor: Theme.of(context).textTheme.bodySmall!.color,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 5),
+                        focusColor: Colors.white,
+                        hintText: "searchDes".tr,
+                      ),
+                    ),
+                    Expanded(
+                      child: Obx(() {
+                        final isEmpty =
+                            searchScreenController.suggestionList.isEmpty;
+                        final list = isEmpty
+                            ? searchScreenController.historyQuerylist.toList()
+                            : searchScreenController.suggestionList.toList();
+                        return ListView.builder(
+                          padding: const EdgeInsets.only(top: 5),
+                          physics: const BouncingScrollPhysics(
+                              parent: AlwaysScrollableScrollPhysics()),
+                          itemCount: list.length,
+                          itemBuilder: (context, index) => ListTile(
+                            contentPadding:
+                                const EdgeInsets.only(left: 10, right: 20),
+                            onTap: () {
+                              Get.toNamed(
+                                  ScreenNavigationSetup.searchResultScreen,
+                                  id: ScreenNavigationSetup.id,
+                                  arguments: list[index]);
+                              searchScreenController
+                                  .addToHistryQueryList(list[index]);
+                            },
+                            leading: isEmpty ? const Icon(Icons.history) : null,
+                            minLeadingWidth: 20,
+                            dense: true,
+                            title: Text(list[index]),
+                            trailing: SizedBox(
+                              width: 80,
+                              child: Row(
+                                children: [
+                                isEmpty? IconButton(
+                                    iconSize: 18,
+                                    splashRadius: 18,
+                                    visualDensity: const VisualDensity(horizontal: -2),
+                                    onPressed: () {
+                                      searchScreenController.removeQueryFromHistory(
+                                                    list[index]);
+                                    },
+                                    icon: Icon(
+                                      Icons.clear,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium!
+                                          .color,
+                                    ),
+                                  ):const SizedBox(width: 40,),
+                                  IconButton(
+                                    iconSize: 20,
+                                    splashRadius: 18,
+                                    visualDensity:
+                                        const VisualDensity(horizontal: -2),
+                                    onPressed: () {
                                       searchScreenController
-                                          .suggestionList[index]);
-                                },
-                                child: Icon(
-                                  Icons.north_west_rounded,
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .titleMedium!
-                                      .color,
-                                ),
+                                          .suggestionInput(list[index]);
+                                    },
+                                    icon: Icon(
+                                      Icons.north_west,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium!
+                                          .color,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          )))
-                ],
+                          ),
+                        );
+                      }),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/ui/screens/Search/search_screen_controller.dart
+++ lib/ui/screens/Search/search_screen_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 
 import '/services/music_service.dart';
 
@@ -7,6 +8,19 @@ class SearchScreenController extends GetxController {
   final textInputController = TextEditingController();
   final musicServices = Get.find<MusicServices>();
   final suggestionList = [].obs;
+  final historyQuerylist = [].obs;
+  late Box<dynamic> queryBox;
+
+  @override
+  onInit() {
+    _init();
+    super.onInit();
+  }
+
+  _init() async {
+    queryBox = await Hive.openBox("searchQuery");
+    historyQuerylist.value = queryBox.values.toList().reversed.toList();
+  }
 
   Future<void> onChanged(String text) async {
     suggestionList.value = await musicServices.getSearchSuggestion(text);
@@ -19,9 +33,26 @@ class SearchScreenController extends GetxController {
     await onChanged(txt);
   }
 
+  Future<void> addToHistryQueryList(String txt) async {
+    if (historyQuerylist.length > 6) {
+      await queryBox.deleteAt(6);
+      historyQuerylist.removeLast();
+    }
+    if (!historyQuerylist.contains(txt)) {
+      await queryBox.add(txt);
+      historyQuerylist.insert(0, txt);
+    }
+  }
+
+  Future<void> removeQueryFromHistory(String txt) async {
+    await queryBox.delete(txt);
+    historyQuerylist.remove(txt);
+  }
+
   @override
   void dispose() {
     textInputController.dispose();
+    queryBox.close();
     super.dispose();
   }
 }

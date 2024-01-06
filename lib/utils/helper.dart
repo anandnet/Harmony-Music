9c13d53049454c98a6cc74bbc1642f0ecd62c497
Anand kumar <anandnet628@gmail.com>
2024-01-06 18:46:55 +0530

    time string fn added

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -118,3 +118,14 @@ Future<bool> newVersionCheck(String currentVersion) async {
     return false;
   }
 }
+
+String getTimeString(Duration time) {
+  final minutes = time.inMinutes.remainder(Duration.minutesPerHour).toString();
+  final seconds = time.inSeconds
+      .remainder(Duration.secondsPerMinute)
+      .toString()
+      .padLeft(2, '0');
+  return time.inHours > 0
+      ? "${time.inHours}:${minutes.padLeft(2, "0")}:$seconds"
+      : "$minutes:$seconds";
+}

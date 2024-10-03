e93305263a6bfeec234c2e3f6d626a3ee1d018ba
anandnet <anandnet628@gmail.com>
2024-10-03 14:50:09 +0530

    Added checks for deleted downloaded songs for desktop

--- lib/utils/house_keeping.dart
+++ lib/utils/house_keeping.dart
@@ -1,6 +1,10 @@
-import 'package:hive/hive.dart';
+import 'dart:io';
 
+import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import '../services/utils.dart';
+import 'helper.dart';
 
 void startHouseKeeping() {
   removeExpiredSongsUrlFromDb();
@@ -13,12 +17,38 @@ Future<void> removeExpiredSongsUrlFromDb() async {
         songsUrlCacheBox.keys.whereType<String>().toList();
     for (var i = 0; i < songsUrlCacheKeysList.length; i++) {
       final songUrlKey = songsUrlCacheKeysList[i];
-      final songUrl = songsUrlCacheBox.get(songUrlKey);
-      if (songUrl == null ||
-          (songUrl != null && isExpired(url: songUrl[0] as String))) {
+      final streamData = songsUrlCacheBox.get(songUrlKey)[1];
+      if (streamData == null ||
+          (streamData != null && isExpired(url: streamData['url'] as String))) {
         await songsUrlCacheBox.delete(songUrlKey);
       }
     }
-    // ignore: empty_catches
-  } catch (e) {}
+  } catch (e) {
+    printERROR("Error in removeExpiredSongsUrlFromDb: $e");
+  } finally {
+    if (GetPlatform.isDesktop) {
+      removeDeletedOfflineSongsFromDb();
+    }
+  }
+}
+
+Future<void> removeDeletedOfflineSongsFromDb() async {
+  final supportDir = (await getApplicationSupportDirectory()).path;
+  try {
+    final songDownloadsBox = Hive.box("SongDownloads");
+    final downloadedSongs = songDownloadsBox.values.toList();
+    for (var i = 0; i < downloadedSongs.length; i++) {
+      final songKey = downloadedSongs[i]['id'];
+      final songUrl = downloadedSongs[i]['url'];
+      if (await File(songUrl).exists() == false) {
+        await songDownloadsBox.delete(songKey);
+        final thumbNailPath = "$supportDir/thumbnails/$songKey.png";
+        if (await File(thumbNailPath).exists()) {
+          await File(thumbNailPath).delete();
+        }
+      }
+    }
+  } catch (e) {
+    printERROR("Error in removeDeletedOfflineSongsFromDb: $e");
+  }
 }

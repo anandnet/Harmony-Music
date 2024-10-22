5e94ed0a79a9cc23f67fb7c173606b34ac01f973
mony <mony170.infinity@gmail.com>
2024-10-22 14:48:29 +0700

    rf-on-main

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,26 @@ import 'package:flutter_ume_kit_console_plus/flutter_ume_kit_console_plus.dart';
 import 'package:flutter_ume_kit_device_plus/flutter_ume_kit_device_plus.dart';
 import 'package:flutter_ume_plus/flutter_ume_plus.dart';
 import 'package:get/get.dart';
-import 'package:harmonymusic/ui/navigator.dart';
+import 'package:harmonymusic/services/audio_handler.dart';
+import 'package:harmonymusic/services/downloader.dart';
+import 'package:harmonymusic/services/music_service.dart';
+import 'package:harmonymusic/services/piped_service.dart';
+import 'package:harmonymusic/ui/home.dart';
+import 'package:harmonymusic/ui/player/player_controller.dart';
+import 'package:harmonymusic/ui/screens/Home/home_screen_controller.dart';
+import 'package:harmonymusic/ui/screens/Library/library_controller.dart';
+import 'package:harmonymusic/ui/screens/Search/search_screen_controller.dart';
+import 'package:harmonymusic/ui/screens/Settings/settings_screen_controller.dart';
+import 'package:harmonymusic/ui/utils/theme_controller.dart';
+import 'package:harmonymusic/utils/app_link_controller.dart';
+import 'package:harmonymusic/utils/get_localization.dart';
+import 'package:harmonymusic/utils/system_tray.dart';
+import 'package:harmonymusic/utils/update_check_flag_file.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:ume_kit_monitor/monitor/awesome_monitor.dart';
 import 'package:ume_kit_monitor/monitor/monitor_action_widget.dart';
 import 'package:ume_kit_monitor/monitor_plugin.dart';
-
-import '/ui/screens/Search/search_screen_controller.dart';
-import '/utils/get_localization.dart';
-import '/services/downloader.dart';
-import '/services/piped_service.dart';
-import 'utils/app_link_controller.dart';
-import '/services/audio_handler.dart';
-import '/services/music_service.dart';
-import '/ui/home.dart';
-import '/ui/player/player_controller.dart';
-import 'ui/screens/Settings/settings_screen_controller.dart';
-import '/ui/utils/theme_controller.dart';
-import 'ui/screens/Home/home_screen_controller.dart';
-import 'ui/screens/Library/library_controller.dart';
-import 'utils/system_tray.dart';
-import 'utils/update_check_flag_file.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,7 +39,7 @@ Future<void> main() async {
     ..register(const MonitorActionsPlugin())
     ..register(Console())
     ..register(const DeviceInfoPanel());
-  runApp(const UMEWidget(enable: true, child: MyApp()));
+  runApp(const UMEWidget(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -53,10 +51,10 @@ class MyApp extends StatelessWidget {
     if (!GetPlatform.isDesktop) Get.put(AppLinksController());
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     SystemChannels.lifecycle.setMessageHandler((msg) async {
-      if (msg == "AppLifecycleState.resumed") {
+      if (msg == 'AppLifecycleState.resumed') {
         SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-      } else if (msg == "AppLifecycleState.detached") {
-        await Get.find<AudioHandler>().customAction("saveSession");
+      } else if (msg == 'AppLifecycleState.detached') {
+        await Get.find<AudioHandler>().customAction('saveSession');
       }
       return null;
     });
@@ -83,10 +81,10 @@ class MyApp extends StatelessWidget {
           home: const Home(),
           debugShowCheckedModeBanner: false,
           translations: Languages(),
-          locale: Locale(Hive.box("AppPrefs").get('currentAppLanguageCode') ?? "en"),
-          fallbackLocale: const Locale("en"),
+          locale: Locale(Hive.box('AppPrefs').get('currentAppLanguageCode') ?? 'en'),
+          fallbackLocale: const Locale('en'),
           builder: (context, child) {
-            final scale = MediaQuery.of(context).textScaler.clamp(minScaleFactor: 1.0, maxScaleFactor: 1.1);
+            final scale = MediaQuery.of(context).textScaler.clamp(minScaleFactor: 1, maxScaleFactor: 1.1);
             return MediaQuery(
               data: MediaQuery.of(context).copyWith(textScaler: scale),
               child: child!,
@@ -97,20 +95,20 @@ class MyApp extends StatelessWidget {
 }
 
 Future<void> startApplicationServices() async {
-  Get.lazyPut(() => PipedServices(), fenix: true);
-  Get.lazyPut(() => MusicServices(), fenix: true);
-  Get.lazyPut(() => ThemeController(), fenix: true);
-  Get.lazyPut(() => PlayerController(), fenix: true);
-  Get.lazyPut(() => HomeScreenController(), fenix: true);
-  Get.lazyPut(() => LibrarySongsController(), fenix: true);
-  Get.lazyPut(() => LibraryPlaylistsController(), fenix: true);
-  Get.lazyPut(() => LibraryAlbumsController(), fenix: true);
-  Get.lazyPut(() => LibraryArtistsController(), fenix: true);
-  Get.lazyPut(() => SettingsScreenController(), fenix: true);
-  Get.lazyPut(() => Downloader(), fenix: true);
+  Get.lazyPut(PipedServices.new, fenix: true);
+  Get.lazyPut(MusicServices.new, fenix: true);
+  Get.lazyPut(ThemeController.new, fenix: true);
+  Get.lazyPut(PlayerController.new, fenix: true);
+  Get.lazyPut(HomeScreenController.new, fenix: true);
+  Get.lazyPut(LibrarySongsController.new, fenix: true);
+  Get.lazyPut(LibraryPlaylistsController.new, fenix: true);
+  Get.lazyPut(LibraryAlbumsController.new, fenix: true);
+  Get.lazyPut(LibraryArtistsController.new, fenix: true);
+  Get.lazyPut(SettingsScreenController.new, fenix: true);
+  Get.lazyPut(Downloader.new, fenix: true);
 
   if (GetPlatform.isDesktop) {
-    Get.lazyPut(() => SearchScreenController(), fenix: true);
+    Get.lazyPut(SearchScreenController.new, fenix: true);
     Get.put(DesktopSystemTray());
   }
 }
@@ -118,29 +116,29 @@ Future<void> startApplicationServices() async {
 initHive() async {
   String applicationDataDirectoryPath;
   if (GetPlatform.isDesktop) {
-    applicationDataDirectoryPath = "${(await getApplicationSupportDirectory()).path}/db";
+    applicationDataDirectoryPath = '${(await getApplicationSupportDirectory()).path}/db';
   } else {
     applicationDataDirectoryPath = (await getApplicationDocumentsDirectory()).path;
   }
   await Hive.initFlutter(applicationDataDirectoryPath);
-  await Hive.openBox("SongsCache");
-  await Hive.openBox("SongDownloads");
+  await Hive.openBox('SongsCache');
+  await Hive.openBox('SongDownloads');
   await Hive.openBox('SongsUrlCache');
-  await Hive.openBox("AppPrefs");
+  await Hive.openBox('AppPrefs');
 }
 
 void _setAppInitPrefs() {
-  final appPrefs = Hive.box("AppPrefs");
+  final appPrefs = Hive.box('AppPrefs');
   if (appPrefs.isEmpty) {
     appPrefs.putAll({
       'themeModeType': 0,
-      "cacheSongs": false,
-      "skipSilenceEnabled": false,
+      'cacheSongs': false,
+      'skipSilenceEnabled': false,
       'streamingQuality': 1,
       'themePrimaryColor': 4278199603,
-      'discoverContentType': "QP",
+      'discoverContentType': 'QP',
       'newVersionVisibility': updateCheckFlag,
-      "cacheHomeScreenData": true
+      'cacheHomeScreenData': true
     });
   }
 }

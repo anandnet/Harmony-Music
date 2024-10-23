894e0345567f44d7a0c5d3bd067ce5f747eaf205
mony <mony170.infinity@gmail.com>
2024-10-23 18:00:54 +0700

    apply-ts-to-create-playlist-dialog

--- lib/ui/widgets/create_playlist_dialog.dart
+++ lib/ui/widgets/create_playlist_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tailwind/flutter_tailwind.dart';
 import 'package:get/get.dart';
 import 'package:harmonymusic/models/playlist.dart';
 import 'package:harmonymusic/services/piped_service.dart';
@@ -9,8 +10,13 @@ import 'package:harmonymusic/ui/widgets/modified_text_field.dart';
 import 'package:harmonymusic/ui/widgets/snackbar.dart';
 
 class CreateNRenamePlaylistPopup extends StatelessWidget {
-  const CreateNRenamePlaylistPopup(
-      {super.key, this.isCreateNadd = false, this.songItems, this.renamePlaylist = false, this.playlist});
+  const CreateNRenamePlaylistPopup({
+    super.key,
+    this.isCreateNadd = false,
+    this.songItems,
+    this.renamePlaylist = false,
+    this.playlist,
+  });
 
   final bool isCreateNadd;
   final bool renamePlaylist;
@@ -19,9 +25,9 @@ class CreateNRenamePlaylistPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final librPlstCntrller = Get.find<LibraryPlaylistsController>();
-    librPlstCntrller.changeCreationMode('local');
-    librPlstCntrller.textInputController.text = '';
+    final libPlaylistCtrl = Get.find<LibraryPlaylistsController>();
+    libPlaylistCtrl.changeCreationMode('local');
+    libPlaylistCtrl.textInputController.text = '';
     final isPipedLinked = Get.find<PipedServices>().isLoggedIn;
     return CommonDialog(
       child: Container(
@@ -29,7 +35,7 @@ class CreateNRenamePlaylistPopup extends StatelessWidget {
         padding: const EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 10),
         child: Stack(
           children: [
-            Column(children: [
+            column.children([
               Padding(
                 padding: const EdgeInsets.only(bottom: 5),
                 child: Align(
@@ -41,102 +47,83 @@ class CreateNRenamePlaylistPopup extends StatelessWidget {
                 ),
               ),
               if (isPipedLinked && !renamePlaylist)
-                Obx(
-                  () => Row(
-                    children: [
-                      Row(
-                        children: [
-                          Radio(
-                              value: 'piped',
-                              groupValue: librPlstCntrller.playlistCreationMode.value,
-                              onChanged: librPlstCntrller.changeCreationMode),
-                          Text('Piped'.tr),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Row(
-                        children: [
-                          Radio(
-                              value: 'local',
-                              groupValue: librPlstCntrller.playlistCreationMode.value,
-                              onChanged: librPlstCntrller.changeCreationMode),
-                          Text('local'.tr),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
+                Obx(() => row.children([
+                      row.children([
+                        Radio(
+                            value: 'piped',
+                            groupValue: libPlaylistCtrl.playlistCreationMode.value,
+                            onChanged: libPlaylistCtrl.changeCreationMode),
+                        'Piped'.tr.text.mk,
+                      ]),
+                      const SizedBox(width: 15),
+                      row.children([
+                        Radio(
+                            value: 'local',
+                            groupValue: libPlaylistCtrl.playlistCreationMode.value,
+                            onChanged: libPlaylistCtrl.changeCreationMode),
+                        'local'.tr.text.mk,
+                      ]),
+                    ])),
               ModifiedTextField(
                 textCapitalization: TextCapitalization.sentences,
                 autofocus: true,
                 cursorColor: Theme.of(context).textTheme.titleSmall!.color,
-                controller: librPlstCntrller.textInputController,
+                controller: libPlaylistCtrl.textInputController,
                 decoration: const InputDecoration(
                   contentPadding: EdgeInsets.only(left: 5),
                   focusColor: Colors.white,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    InkWell(
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text('cancel'.tr),
-                      ),
-                      onTap: () => Navigator.of(context).pop(),
-                    ),
-                    DecoratedBox(
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).textTheme.titleLarge!.color,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: InkWell(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                          child: Text(
-                            isCreateNadd
-                                ? 'createnAdd'.tr
-                                : renamePlaylist
-                                    ? 'rename'.tr
-                                    : 'create'.tr,
-                            style: TextStyle(color: Theme.of(context).canvasColor),
-                          ),
+              padding.pt44.child(
+                row.spaceAround.children([
+                  InkWell(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: padding.p10.child('cancel'.tr.text.mk),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).textTheme.titleLarge!.color, borderRadius: BorderRadius.circular(10)),
+                    child: InkWell(
+                      child: padding.ph30.pv20.child(
+                        Text(
+                          isCreateNadd
+                              ? 'createnAdd'.tr
+                              : renamePlaylist
+                                  ? 'rename'.tr
+                                  : 'create'.tr,
+                          style: TextStyle(color: Theme.of(context).canvasColor),
                         ),
-                        onTap: () async {
-                          if (renamePlaylist) {
-                            librPlstCntrller.renamePlaylist(playlist!).then((value) {
-                              if (value) {
-                                if (!context.mounted) return;
-                                Navigator.of(context).pop();
-                                ScaffoldMessenger.of(context).showSnackBar(snackbar(context, 'playlistRenameAlert'.tr));
-                              }
-                            });
-                          } else {
-                            librPlstCntrller
-                                .createNewPlaylist(createPlaylistNaddSong: isCreateNadd, songItems: songItems)
-                                .then((value) {
-                              if (!context.mounted) return;
-                              if (value) {
-                                ScaffoldMessenger.of(context).showSnackBar(snackbar(context,
-                                    isCreateNadd ? 'playlistCreatednsongAddedAlert'.tr : 'playlistCreatedAlert'.tr));
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(snackbar(context, 'errorOccuredAlert'.tr));
-                              }
-                              Navigator.of(context).pop();
-                            });
-                          }
-                        },
                       ),
+                      onTap: () async {
+                        if (renamePlaylist) {
+                          libPlaylistCtrl.renamePlaylist(playlist!).then((value) {
+                            if (value) {
+                              if (!context.mounted) return;
+                              Navigator.of(context).pop();
+                              ScaffoldMessenger.of(context).showSnackBar(snackbar(context, 'playlistRenameAlert'.tr));
+                            }
+                          });
+                        } else {
+                          libPlaylistCtrl
+                              .createNewPlaylist(createPlaylistNaddSong: isCreateNadd, songItems: songItems)
+                              .then((value) {
+                            if (!context.mounted) return;
+                            if (value) {
+                              ScaffoldMessenger.of(context).showSnackBar(snackbar(context,
+                                  isCreateNadd ? 'playlistCreatednsongAddedAlert'.tr : 'playlistCreatedAlert'.tr));
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(snackbar(context, 'errorOccuredAlert'.tr));
+                            }
+                            Navigator.of(context).pop();
+                          });
+                        }
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ]),
               )
             ]),
-            Obx(() => (librPlstCntrller.creationInProgress.isTrue && isPipedLinked)
+            Obx(() => (libPlaylistCtrl.creationInProgress.isTrue && isPipedLinked)
                 ? const Positioned(
                     top: 5,
                     right: 8,
